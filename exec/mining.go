745d35857ff457db2f7372cfc13e75b913d4a78e
Haswell <haswell@HaswelldeMacBook-Air.local>
2024-06-17 13:41:49 +0800

    加入上次未保存的更改

--- exec/mining.go
+++ exec/mining.go
@@ -6,6 +6,6 @@ import (
 )
 
 // 打包多个交易并生成新区块，生成过程中应不断调整区块nonce值以满足PoW约束
-func MiningNewBlock([]entity.Transaction) ds.Block {
+func miningNewBlock([]entity.Transaction) ds.Block {
 	return ds.Block{}
 }
