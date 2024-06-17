9e459066db18194103aa8324dadebd7af0b034b5
Haswell <haswell@HaswelldeMacBook-Air.local>
2024-06-17 13:36:05 +0800

    完善了cli和comm的指责范围

--- cli/cli.go
+++ cli/cli.go
@@ -1,22 +1,27 @@
 // 本包应当通过http请求的方式调用/comm给出的web地址
+// 接收键盘输入并对应到需要调用的函数
 package cli
 
 import "github.com/Homebrew-Blockchain-Club/minichain/entity"
 
-type cli struct {
+type CLI struct {
+}
+
+func NewCLI() *CLI {
+	return nil
 }
 
 // 根据账户地址返回账户信息 若不存在则返回nil
-func QueryAccount([]byte) *entity.Account {
+func (*CLI) QueryAccount([]byte) *entity.Account {
 	return nil
 }
 
 // 转账、创建合约、调用合约共用的函数
-func NewTransaction(privkey []byte, to []byte, amount []byte, data []byte, gaslimit uint64) {
+func (*CLI) NewTransaction(privkey []byte, to []byte, amount []byte, data []byte, gaslimit uint64) {
 
 }
 
 // 创建新的公私钥对
-func NewAccount() ([]byte, []byte) {
+func (*CLI) NewAccount() ([]byte, []byte) {
 	return nil, nil
 }
--- main.go
+++ main.go
@@ -1,3 +1,11 @@
 package minichain
 
-func main() {}
+import (
+	"github.com/Homebrew-Blockchain-Club/minichain/cli"
+	"github.com/Homebrew-Blockchain-Club/minichain/comm"
+)
+
+func main() {
+	cli.NewCLI()
+	comm.NewCommunicator()
+}
