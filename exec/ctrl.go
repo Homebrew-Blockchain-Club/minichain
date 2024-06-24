0518229575a8a086feab4a891a5285f0f1926c19
luobo177 <128736213+luobo177@users.noreply.github.com>
2024-06-24 20:31:33 +0800

    第二次更改ctrl,将mpt结构图成员删去，改为从gettop得到区块获得mpt，再进行接下来 的操作，同时addblock函数返回挖出的区块改为直接settop区块

--- exec/ctrl.go
+++ exec/ctrl.go
@@ -8,9 +8,10 @@ import (
 	"github.com/Homebrew-Blockchain-Club/minichain/typeconv"
 )
 
+const isFull = true
+
 // 用于核心控制的结构体，上层包应当直接调用这里的函数，其他地方的函数不对包外公开
 type Controller struct {
-	mpt  *ds.MPT
 	pool *TxPool
 }
 
@@ -19,17 +20,18 @@ func NewController() *Controller {
 }
 
 // 加入新交易。在交易池未满时放入交易池，交易池已满则进行挖矿，挖矿完成则将新区块返回到Communicator来发布
-func (c *Controller) AddTransaction(tx entity.Transaction) (ds.Block, bool) { //满了的话返回true，并返回区块，没满则返回空区块，和false
-	if c.pool.isFull() {
+func (c *Controller) AddTransaction(tx entity.Transaction) int { //满了的话返回1，并将区块加入，没满则返回0
+	if isFull { //isFull函数满足后改为isFull函数
 		var tx []entity.Transaction //交易从何而来？
 		for c.pool.Length() != 0 {
 			tx = append(tx, c.pool.Poll()) //交易从pool弹出来
 		}
 		var block = miningNewBlock(tx)
-		return block, true
+		ds.SetTop(&block)
+		return 1
 	} else {
 		c.pool.Insert(tx)
-		return ds.Block{}, false
+		return 0
 	}
 }
 
@@ -37,7 +39,7 @@ func (c *Controller) AddTransaction(tx entity.Transaction) (ds.Block, bool) { //
 // 验证流程：1.满足PoW约束 2.MPT证明通过
 func (c *Controller) AddBlock(block ds.Block) bool {
 	if check(hasher.Hash(typeconv.ToBytes(block.Header)), 5) {
-		if c.mpt.Proof() {
+		if ds.GetMPT(block.Header.StateRoot).Proof() {
 			ds.SetTop(&block)
 			return true
 		} else {
@@ -50,7 +52,7 @@ func (c *Controller) AddBlock(block ds.Block) bool {
 
 // 询问账号信息，从/ds中得到账号信息并返回
 func (c *Controller) QueryAccount(key []byte) entity.Account {
-	var byteAccount = c.mpt.Query(key)
+	var byteAccount = ds.GetMPT(ds.GetTop().Header.StateRoot).Query(key)
 	var account = typeconv.FromBytes[entity.Account](byteAccount)
 	return account
 }
